a6e4a1984404a6e3e5be299a9dc3de9ca6a8b861
Dev Server <developers@almanapp.nl>
2017-07-27 19:29:58 +0200

    First

---
+++ Readme.md
@@ -0,0 +1,23 @@
+# Debugert
+
+
+## Install:
+
+
+ - Install NodeJS https://www.digitalocean.com/community/tutorials/how-to-install-node-js-on-ubuntu-16-04
+ - Also install npm (usually gets installed with nodejs)
+ - Clone this repo `git clone https://github.com/spidfire/simple-debug.git` or `git clone git@github.com:spidfire/simple-debug.git`
+ - Go to the diectory `cd simple-debug`
+ - Install packages `npm install`
+
+
+## Run
+
+Just run `node debugger.js`
+
+## Fancy install
+
+https://www.digitalocean.com/community/tutorials/how-to-use-pm2-to-setup-a-node-js-production-environment-on-an-ubuntu-vps
+
+
+Kind Regards, Spidfire
---
+++ debugger.js
@@ -0,0 +1,55 @@
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
+
+
+// parse application/json
+app.use(bodyParser.json())
+server.listen(8080);
+
+
+
+console.log("Listning on http://debugger.almanapp.nl:8080/")
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+var previous = [];
+
+app.post('/test', function (req, res) {
+  if (!req.body) return res.sendStatus(400)
+  var data = req.body
+  console.log(data)
+  io.local.emit('news',  data)
+  previous.push(data)
+  if(previous.length > 10){
+    previous.shift()
+  }
+  res.send("OK");
+});
+
+io.on('connection', function (socket) {
+  for (var i = 0; i < previous.length; i++) {
+
+    socket.emit('news', previous[i]);
+  }
+
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
+
+
+// val debug_params = HashMap<String, String>()
+// debug_params.put("request_test_type", str_method)
+// debug_params.put("request_test_checksystem", query_string)
+// debug_params.put("identifier", AppController.unique_tag)
+// debug_params.put("uuid", parameters["uuid"] ?: "unknown")
+// parameters.toList().forEach { debug_params.put(it.name, it.value) }
+// val proxy_query_string = "https://dev.almanapp.nl/checksystem/debug/insert/"
+
+
